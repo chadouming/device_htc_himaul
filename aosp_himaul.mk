a79be9734e8e20539e6827e4163f2ef9e824fdd6
Chad Cormier Roussel <chadcormierroussel@gmail.com>
2015-10-28 19:13:30 -0400

    Update for uber

--- aosp_himaul.mk
+++ aosp_himaul.mk
@@ -16,14 +16,14 @@
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
 
 # Inherit from himaul device
 $(call inherit-product, device/htc/himaul/device.mk)
 
 # Set those variables here to overwrite the inherited values.
 PRODUCT_DEVICE := himaul
-PRODUCT_NAME := full_himaul
+PRODUCT_NAME := aosp_himaul
 PRODUCT_BRAND := htc
 PRODUCT_MODEL := HTC One M9
 PRODUCT_MANUFACTURER := HTC
